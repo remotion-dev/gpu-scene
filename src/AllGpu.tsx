8d45bd7563f20dac4e3dfa4c0ba659060cadf897
Jonny Burger <jonathanburger11@gmail.com>
2025-01-10 19:23:53 +0100

    add back scenes

--- src/AllGpu.tsx
+++ src/AllGpu.tsx
@@ -10,6 +10,34 @@ export const AllGpu: React.FC = () => {
 			<AbsoluteFill
 				style={{
 					transform: `scale(0.5)`,
+					transformOrigin: '0 0',
+				}}
+			>
+				<Scene
+					deviceType="phone"
+					phoneColor="rgba(110, 152, 191, 0.00)"
+					baseScale={1}
+				/>
+			</AbsoluteFill>
+			<AbsoluteFill
+				style={{
+					transform: `scale(0.5)`,
+					transformOrigin: '0 100%',
+				}}
+			>
+				<OrbScene />
+			</AbsoluteFill>
+			<AbsoluteFill
+				style={{
+					transform: `scale(0.5)`,
+					transformOrigin: '100% 100%',
+				}}
+			>
+				<Shadows />
+			</AbsoluteFill>
+			<AbsoluteFill
+				style={{
+					transform: `scale(0.5)`,
 					transformOrigin: '100% 0%',
 				}}
 			>
